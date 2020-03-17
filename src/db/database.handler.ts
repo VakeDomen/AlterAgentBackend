204b2e7a72aef73cca22b87d5f366e8c56d0a43b
Vake Domen <vake.domen94@gmail.com>
2020-03-17 13:05:50 +0100

    added generic type to database handler; added route for querying clilents by tags

--- src/db/database.handler.ts
+++ src/db/database.handler.ts
@@ -4,7 +4,7 @@ import * as config from './config.json';
 
 const dbConnection: Promise<any> = sqlite.open(process.env.SQLITE_DB || './src/db/data/sqlite.db', { Promise });
 
-export async function query(query: string): Promise<DbItem[]> {
+export async function query<T>(query: string): Promise<T[]> {
     const db = await dbConnection;
     if (config.db.log) {
         console.log(query);
@@ -12,30 +12,30 @@ export async function query(query: string): Promise<DbItem[]> {
     return db.all(query);
 }
 
-export async function fetch(table: string, filter: DbItem): Promise<DbItem[]> {
-    return query('SELECT * FROM ' + table + ' WHERE ' + filter.whereString());
+export async function fetch<T>(table: string, filter: DbItem): Promise<T[]> {
+    return query<T>('SELECT * FROM ' + table + ' WHERE ' + filter.whereString());
 }
 
-export async function fetchAll(table: string): Promise<DbItem[]> {
-    return query('SELECT * FROM ' + table);
+export async function fetchAll<T>(table: string): Promise<T[]> {
+    return query<T>('SELECT * FROM ' + table);
 }
 
-export async function insert(table: string, filter: DbItem): Promise<DbItem[]> {
-    return query('INSERT INTO ' + table + ' (' + filter.listKeys() + ') VALUES (' + filter.listValues() + ')');
+export async function insert<T>(table: string, filter: DbItem): Promise<T[]> {
+    return query<T>('INSERT INTO ' + table + ' (' + filter.listKeys() + ') VALUES (' + filter.listValues() + ')');
 }
 
-export async function update(table: string, filter: DbItem): Promise<DbItem[]> {
-    return query('UPDATE ' + table + ' SET ' + filter.valuesToString() + ' WHERE id=\'' + filter.id + '\'');
+export async function update<T>(table: string, filter: DbItem): Promise<T[]> {
+    return query<T>('UPDATE ' + table + ' SET ' + filter.valuesToString() + ' WHERE id=\'' + filter.id + '\'');
 }
 
-export async function deleteItem(table: string, filter: DbItem): Promise<DbItem[]> {
-    return query('DELETE FROM ' + table + ' WHERE ' + filter.whereString());
+export async function deleteItem<T>(table: string, filter: DbItem): Promise<T[]> {
+    return query<T>('DELETE FROM ' + table + ' WHERE ' + filter.whereString());
 }
 
-export async function innerJoin(t1: string, t2: string, t1key: string, t2key: string, filter: DbItem): Promise<DbItem[]> {
-    return query('SELECT * FROM ' + t1 + ' AS t1 INNER JOIN ' + t2 + ' as t2 ON t1.' + t1key + ' = t2.' + t2key + ' WHERE ' + filter.whereString() );
+export async function innerJoin<T>(t1: string, t2: string, t1key: string, t2key: string, filter: DbItem): Promise<T[]> {
+    return query<T>('SELECT * FROM ' + t1 + ' AS t1 INNER JOIN ' + t2 + ' as t2 ON t1.' + t1key + ' = t2.' + t2key + ' WHERE ' + filter.whereString() );
 }
 
-export async function leftJoin(t1: string, t2: string, t1key: string, t2key: string, filter: DbItem): Promise<DbItem[]> {
-    return query('SELECT * FROM ' + t1 + ' AS t1 LEFT JOIN ' + t2 + ' as t2 ON t1.' + t1key + ' = t2.' + t2key + ' WHERE ' + filter.whereString());
+export async function leftJoin<T>(t1: string, t2: string, t1key: string, t2key: string, filter: DbItem): Promise<T[]> {
+    return query<T>('SELECT * FROM ' + t1 + ' AS t1 LEFT JOIN ' + t2 + ' as t2 ON t1.' + t1key + ' = t2.' + t2key + ' WHERE ' + filter.whereString());
 }
--- src/routes/clients.ts
+++ src/routes/clients.ts
@@ -7,6 +7,7 @@ import * as express from 'express';
 import { fetch, insert, update, deleteItem } from '../db/database.handler';
 import * as conf from '../db/config.json';
 import { SuccessResponse } from '../models/success.response';
+import { DbItem } from '../models/db.item';
 
 const router: express.Router = express.Router();
 
@@ -17,6 +18,23 @@ router.get("/clients", auth, async (req: express.Request, resp: express.Response
     new SuccessResponse().setData(data).send(resp);
 });
 
+router.get('/clients/tag/:tagId', auth, async (req: express.Request, resp: express.Response) => {
+    let data = [];
+    const client_tags: ClientTag[] | void = await fetch<ClientTag>(conf.db.tables.client_tags, new ClientTag({tag_id: req.params['tagId']})).catch(err => {
+        new ErrorResponse().setError(err).send(resp);
+    });
+    if (client_tags) {
+        const promises: Promise<Client[]>[] = [];
+        for (const clientTag of client_tags) {
+            promises.push(fetch<Client>(conf.db.tables.clients, new Client({id: clientTag.client_id })));
+        }
+        Promise.all(promises)
+        .then(payload => {new SuccessResponse().setData(payload).send(resp)})
+        .catch(err => {new ErrorResponse().setError(err).send(resp)});
+    } else {
+        new ErrorResponse().send(resp);
+    }
+});
 
 router.get("/clients/:id", auth, async (req: express.Request, resp: express.Response) => {
     const data = await fetch(conf.db.tables.clients, new Client({id: req.params['id']})).catch(err => {
